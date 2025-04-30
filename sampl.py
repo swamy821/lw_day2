861453f0e6d1969945d2d29da9ea056bf27e701d
swamy821 <swamy90355@gmail.com>
2025-04-30 16:32:51 +0530

    diamond pattern

---
+++ sampl.py
@@ -0,0 +1,9 @@
+
+
+n = int(input("Enter the number of rows (half of diamond height): "))
+
+for i in range(n):
+    print(" " * (n - i - 1) + "*" * (2 * i + 1))
+
+for i in range(n - 2, -1, -1):
+    print(" " * (n - i - 1) + "*" * (2 * i + 1))
